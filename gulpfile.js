8ab347df7db5af42db4165a3ce362cca187dae3b
ramitmukherjee <ramit.mkj6@gmail.com>
2020-04-11 20:04:41 +0530

    public and private tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-function defaultTask(cb) {
-    console.log("default task running.");
+const gulp = require('gulp');
+
+// Private task coz not added to exports array
+function clean(cb) {
+
+    console.log("Cleaning...");
+    // call cb() to signal task finish
+    cb();
+
+}
+
+// Public task coz its added to the exports array
+function build(cb) {
+    console.log("Building...");
+    // call cb() to signal task finish
     cb();
 }
 
-exports.default = defaultTask;
+// Public task
+exports.build = build;
+// First clean then build
+exports.default = gulp.series(clean, build);
