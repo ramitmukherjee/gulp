bb0314ed96178cf2e4581928093873190041f7ac
ramitmukherjee <ramit.mkj6@gmail.com>
2020-04-12 08:32:18 +0530

    parallel taskes with promise

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,24 @@ const { series, parallel } = require('gulp');
 function clean(cb) {
 
     // Call this to signal task end
-    cb();
+    return Promise.resolve("cleaned ✨")
+        .then((data) => console.log(data));
 }
 
 function css(cb) {
     
     // Call this to signal task end
-    cb();
+    return Promise.resolve("minify css ⭐ ")
+        .then((data) => console.log(data));
 }
 
 function javascript(cb) {
 
     // Call this to signal task end
-    cb();
+    return Promise.resolve("minify js ⭕")
+        .then((data) => console.log(data));
 }
 
-// first run clean and then run css and javascript in parallel
+// first run clean and when clean is finished
+// then run both css and javascript in parallel
 exports.build = series(clean, parallel(css, javascript));
